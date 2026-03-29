168da94efefa1fd84aed52bcf4eabd8094d36060
Apexo <git@apexo.de>
2026-03-29 11:29:34 +0200

    cleanup

--- src/peripherals/avrdx-wdt.ts
+++ src/peripherals/avrdx-wdt.ts
@@ -8,15 +8,5 @@ import type { CPU } from 'avr8js/cpu/cpu';
 
 export class AVRDxWDT {
   constructor(cpu: CPU, base: number) {
-    // firmware writes 0 to disable WDT. We need a write hook so it doesn't crash.
-    // cpu.writeHooks[base + CTRLA] = (value) => {
-    //   this.cpu.data[base + CTRLA] = value;
-    //   return true;
-    // };
-
-    // cpu.writeHooks[base + STATUS] = (value) => {
-    //   this.cpu.data[base + STATUS] = value;
-    //   return true;
-    // };
   }
 }
